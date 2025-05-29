7612782adb8f998d8da90a10f4880580dd411ff9
Daniellllracz <78079867+Daniellllracz@users.noreply.github.com>
2025-05-29 18:33:24 +0200

    Add files via upload

---
+++ EUtazas2020/EUtazas2020/Berteletek.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EUtazas2020
+{
+    internal class Berteletek
+    {
+        // A megálló sorszáma, ahol az utas felszállt (0-29)
+        public int Megallo { get; set; }
+
+        // A felszállás dátuma és időpontja (pl. 20190326-0700 => 2019.03.26 07:00)
+        public DateTime FelSzDatumI { get; set; }
+
+        // A kártya azonosítója (7 jegyű szám)
+        public int KartyaID { get; set; }
+
+        // A jegy vagy bérlet típusa (pl. JGY, FEB, TAB, stb.)
+        public string JegyVBerlet { get; set; }
+
+        // Bérlet esetén a lejárat dátuma (ééééhhnn), jegy esetén a felhasználható jegyek száma (0-10)
+        public string BerletErvenyesege { get; set; }
+
+        // Konstruktor, amely feldolgozza az egy sort az utasadat.txt állományból
+        public Berteletek(string sor)
+        {
+            // Sor felbontása szóközök mentén
+            string[] adatok = sor.Split(' ');
+
+            // 1. adat: megálló száma
+            Megallo = int.Parse(adatok[0]);
+
+            // 2. adat: felszállás dátuma és időpontja, például: "20190326-0700"
+            FelSzDatumI = DateTime.ParseExact(adatok[1], "yyyyMMdd-HHmm", null);
+
+            // 3. adat: kártya azonosítója
+            KartyaID = int.Parse(adatok[2]);
+
+            // 4. adat: jegy vagy bérlet típusa
+            JegyVBerlet = adatok[3];
+
+            // 5. adat: bérlet érvényességi dátuma VAGY jegyek száma
+            BerletErvenyesege = adatok[4];
+        }
+    }
+}
---
+++ EUtazas2020/EUtazas2020/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+
+namespace EUtazas2020;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        // 1. Lista az összes utas adatának tárolására
+        List<Berteletek> utasok = new List<Berteletek>();
+
+        // 2. Fájl beolvasása soronként
+        foreach (var sor in File.ReadLines(@"../../../src/utasadat.txt"))
+        {
+            utasok.Add(new Berteletek(sor));
+        }
+
+        // 3. Kiírjuk, hány utas szeretett volna felszállni
+        Console.WriteLine($"3. feladat:\n {utasok.Count} utas szeretett volna felszállni a buszra.");
+        
+        int elutasitott = 0;
+
+        foreach (var utas in utasok)
+        {
+            if (utas.JegyVBerlet == "JGY")
+            {
+                // Jegyek száma
+                int jegyekSzama = int.Parse(utas.BerletErvenyesege);
+                if (jegyekSzama == 0)
+                {
+                    elutasitott++;
+                }
+            }
+            else
+            {
+                // Bérlet esetén ellenőrizzük a dátumot
+                DateTime ervenyessegDatuma = DateTime.ParseExact(utas.BerletErvenyesege, "yyyyMMdd", CultureInfo.InvariantCulture);
+
+                if (ervenyessegDatuma < utas.FelSzDatumI.Date)
+                {
+                    elutasitott++;
+                }
+            }
+        }
+
+        Console.WriteLine($"4. feladat:\n {elutasitott} utast kellett elutasítani.");
+        // 5. feladat: Legforgalmasabb megálló meghatározása
+        Dictionary<int, int> megalloStat = new Dictionary<int, int>();
+
+        foreach (var utas in utasok)
+        {
+            if (!megalloStat.ContainsKey(utas.Megallo))
+            {
+                megalloStat[utas.Megallo] = 0;
+            }
+            megalloStat[utas.Megallo]++;
+        }
+
+        // Megkeressük a legtöbb felszállási kísérletet
+        int legtobb = megalloStat.Values.Max();
+
+        // Kiválasztjuk a legkisebb sorszámú megállót, ahol ez történt
+        int legtobbMegallo = megalloStat
+            .Where(kvp => kvp.Value == legtobb)
+            .Select(kvp => kvp.Key)
+            .Min();
+
+        Console.WriteLine($"5. feladat:\n A legtöbb utas ({legtobb} fő) a {legtobbMegallo}. megállóban próbált felszállni.");
+
+        // 6. feladat: Kedvezményes és ingyenes utazók száma
+        int kedvezmenyes = 0;
+        int ingyenes = 0;
+
+        foreach (var utas in utasok)
+        {
+            if (utas.JegyVBerlet == "JGY") continue; // Jegy -> nem bérlet, kihagyjuk
+
+            // Bérlet érvényesség ellenőrzése
+            DateTime ervenyesseg;
+            bool datumOK = DateTime.TryParseExact(utas.BerletErvenyesege, "yyyyMMdd", CultureInfo.InvariantCulture, DateTimeStyles.None, out ervenyesseg);
+
+            if (datumOK && ervenyesseg >= utas.FelSzDatumI.Date)
+            {
+                switch (utas.JegyVBerlet)
+                {
+                    case "TAB":
+                    case "NYB":
+                        kedvezmenyes++;
+                        break;
+                    case "NYP":
+                    case "RVS":
+                    case "GYK":
+                        ingyenes++;
+                        break;
+                }
+            }
+
+        }
+
+        Console.WriteLine($"6. feladat:\n Kedvezményesen utazók száma: {kedvezmenyes} fő");
+        Console.WriteLine($"6. feladat:\n Ingyenesen utazók száma: {ingyenes} fő");
+
+        // 7. feladat: Figyelmeztetés 3 napon belül lejáró érvényes bérlet esetén
+        using StreamWriter sw = new(@"../../../src/figyelmeztetes.txt");
+
+        foreach (var utas in utasok)
+        {
+            if (utas.JegyVBerlet == "JGY") continue; // nem bérlet
+            if (!DateTime.TryParseExact(utas.BerletErvenyesege, "yyyyMMdd", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime ervenyesseg))
+                continue;
+
+            if (ervenyesseg >= utas.FelSzDatumI.Date) // még érvényes
+            {
+                TimeSpan hatralevo = ervenyesseg - utas.FelSzDatumI.Date;
+                if (hatralevo.TotalDays <= 3)
+                {
+                    string ervenyesFormazva = ervenyesseg.ToString("yyyy-MM-dd");
+                    sw.WriteLine($"{utas.KartyaID} {ervenyesFormazva}");
+                }
+            }
+        }
+
+        Console.WriteLine("7. feladat:\n A figyelmeztetésre jogosult utasok kiírva a figyelmeztetes.txt fájlba.");
+    }
+}
